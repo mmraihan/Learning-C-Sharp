f4f8c7254508ee80135d6103fa8fd71a937295aa
mmraihan <mm_raihan@live.com>
2021-07-14 01:06:34 +0600

    Class 2 Done

---
+++ Examples/BasicSyntax/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace BasicSyntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hi");
+
+            //int i = Console.Read();
+            //Console.WriteLine(i);
+
+            //string s = Console.ReadLine();
+            //Console.WriteLine(s);
+
+            string line = Console.ReadLine();
+
+            string[] lineParts= line.Split(',');
+            for (int i = 0; i < lineParts.Length; i++)
+            {
+                Console.Write(lineParts[i]);
+            }
+
+
+
+
+            
+           // Console.ReadKey();
+        }
+    }
+}
