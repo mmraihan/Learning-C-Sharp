cf9c515e2b6a6d1ad5de8d80fb45971046990b00
mmraihan <mm_raihan@live.com>
2021-07-17 23:35:34 +0600

    If else

--- Examples/ControlFlow/Program.cs
+++ Examples/ControlFlow/Program.cs
@@ -6,7 +6,41 @@ namespace ControlFlow
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //////////////////////////////////////////////if else
+            //int hour = 10;
+
+            //if (hour>0 && hour<12)
+            //{
+            //    Console.WriteLine("Its Morning");
+            //}
+            //else if (hour>12 && hour<18)
+            //{
+            //    Console.WriteLine("AfterNoon");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Evening");
+            //}
+
+
+            ///////////////////////////////////////////////////////////// if else
+            bool isCustomer = true;
+
+            //float price;
+            //if (isCustomer)
+            //    price = 19.00f;
+            //else
+            //    price = 222.2f;
+
+            //Console.WriteLine(price);
+
+            float price = (isCustomer) ? 19.0f : 26.77f;
+
+            Console.WriteLine(price);
+                
+                
+         
         }
     }
 }
