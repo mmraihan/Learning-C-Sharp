353596ad47cee25b9069b44ba630ded46dddb89c
mmraihan <mm_raihan@live.com>
2021-08-08 16:16:17 +0600

    Different type of methods

--- Examples/List/Program.cs
+++ Examples/List/Program.cs
@@ -11,96 +11,22 @@ namespace List
     {
         static void Main(string[] args)
         {
-            //var Nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-            //for (int i = 0; i < Nums.Length; i++)
-            //{
-            //    if (i % 2 == 0)
-
-            //        Console.WriteLine(i);
-
-            //}
-
-            //var i = 0;
-            //while (i <= 10)
-            //{
-            //    if (i % 2 == 0) ;
-
-            //    Console.WriteLine(i);
-            //    i++;
-
-            //}
-
-            //while (true)
-            //{
-            //    Console.Write("Type your Name: ");
-            //   var input= Console.ReadLine();
-
-            //    if (!String.IsNullOrWhiteSpace(input))
-            //    {
-            //        Console.WriteLine("@Echo: " + input);
-            //        continue;
-            //    }
-
-
-            //    break;
-            //}
-
-            //int i = 10, j = 20;
-
-            //if (i < j)
-            //{
-            //    Console.WriteLine("i is less than j");
-            //}
-
-            //if (i > j)
-            //{
-            //    Console.WriteLine("i is greater than j");
-            //}
-
-            //int i = 10, j = 20;
-
-            //if (i > 1)
-            //{
-            //    Console.WriteLine("i is less than j");
-            //}
-
-            //if (i <j)
-            //{
-            //    Console.WriteLine("i is greater than j");
-            //}
-
-            //Nested if else statements
-
-            //int i = 20, j = 20;
-            //if (i!=j)
-            //{
-            //    if (i<j)
-            //    {
-            //        Console.WriteLine("I is less tna j");
-            //    }
-            //    else if (i > j)
-            //    {
-            //        Console.WriteLine("I is greater than j");
-            //    }
-            //}
-
-            //else
-            //{
-            //    Console.WriteLine("I is equal to j");
-            //};
-
-            int i = 0;
-
-            while (true)
+            int num = 123;
+            int result = 0;
+            int rem;
+            while (num!=0)
             {
-                Console.WriteLine("i = {0}", i);
+                rem = num % 10;
+                result = result*10 + rem;
+                num = num / 10;
 
-                i++;
-
-                if (i < 10) ;
-                break;
             }
+
+            Console.WriteLine(result);
+
         }
     }
+
+    
 }
---
+++ Examples/StringAndCharacters/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace StringAndCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region String Method
+            string address = "Mirpur, Dhaka,Bangladesh.";
+
+            Console.WriteLine("Contains k: "+ address.Contains('k'));
+
+            Console.WriteLine(address.ToLower());
+            Console.WriteLine(address.ToUpper());
+
+            Console.WriteLine(address.StartsWith('d'));
+            Console.WriteLine("End With SH: "+ address.EndsWith("SH", StringComparison.OrdinalIgnoreCase));
+
+            int commaIndex = address.IndexOf(',');
+            commaIndex = address.IndexOf(',', 7);
+            Console.WriteLine("Comma Index: " + commaIndex);
+
+            int indexOfAny = address.IndexOfAny(new char[] { ',', '.', ';' });
+            int indexOfAny2 = address.IndexOfAny(new char[] { ',', '.', ';' }, 7,10);
+
+            Console.WriteLine($"IndexOfAny: {indexOfAny}, IndexOfAny 2: {indexOfAny2} ");
+
+            int pos = 0;
+            while (pos>=0)
+            {
+                pos= address.IndexOfAny(new char[] { ',', '.', ';' }, pos+1);
+                if (pos>=0)            
+                Console.WriteLine(pos);
+            }
+
+
+             address=address.Insert(7, "--");
+
+            Console.WriteLine(address);
+
+            address = address.PadRight(50, '*');
+            Console.WriteLine(address);
+
+            address = address.Remove(2, 10);
+            Console.WriteLine(address);
+
+            address = "Mirpur, Dhaka";
+            address = address.Replace('M', 'm');
+            Console.WriteLine(address);
+           
+            string[] parts= address.Split(',');
+
+            foreach (var item in parts)
+            {
+                Console.WriteLine(item);
+            }
+
+            var trimedPart1=parts[1].Trim();
+            Console.WriteLine(trimedPart1);
+
+
+            address = "***** Mirpur2,Dhaka2";
+            address= address.Trim('*', ' '); 
+
+            Console.WriteLine(address);
+
+            char[] letters = address.ToCharArray();
+
+            var text = string.Empty;
+            Console.WriteLine(text);
+            text=string.Concat("Hello");
+            Console.WriteLine(text);
+
+           bool text2= string.IsNullOrEmpty(text);
+            Console.WriteLine(text2);
+
+            bool text3=string.IsNullOrWhiteSpace(text);
+            Console.WriteLine(text3);
+
+            Console.WriteLine(string.Join('q', parts));
+
+            Console.WriteLine(char.ToUpper('c'));
+
+
+
+            #endregion
+
+            #region Character Methods
+
+
+
+
+
+
+
+
+
+
+            #endregion
+        }
+
+
+
+    }
+}
