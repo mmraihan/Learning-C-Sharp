ae5a5b56460bf2189715b1ad0a13fdf86d9b69cb
mmraihan <mm_raihan@live.com>
2021-08-10 14:12:41 +0600

    Method -1

---
+++ Examples/Methods/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Methods
+{
+    public class Calculator
+    {
+
+        int num1 = 10;
+        int num2 = 20;
+        int result;
+
+        void Add()
+        {
+            result = num1 + num2;
+            DisplayResult();
+        }
+
+        void Subtract()
+        {
+            result = num1 - num2;
+            DisplayResult();
+        }
+        void DisplayResult()
+        {
+            Console.WriteLine(result);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Calculator obj = new Calculator();
+            obj.Add();
+            obj.Subtract();
+        
+
+        }
+    }
+}
