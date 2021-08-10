4f4d70410cfaf0d5549c53f1d1b39ac0f17b1d09
mmraihan <mm_raihan@live.com>
2021-08-10 18:33:40 +0600

    Classes -1 (With Field)

---
+++ Examples/Classes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student(); /// Student()= Constructor , Special kind of method
+            student.name = "Raihan";
+            student.dateOfBirth = new DateTime(1995, 5, 10);
+            student.cgpa = 3.02;
+            student.address = "Bangladesh";
+
+            var cgpaResult= student.UpdateCgpa(3.08);
+
+            Console.WriteLine("Raihan's CGPA: " + cgpaResult);
+            Console.WriteLine(student.dateOfBirth);
+
+
+
+            Student student2 = new Student();
+            student2.name = "Riya";
+            student2.cgpa = 3.57;
+            student2.UpdateCgpa(3.92);
+            Console.WriteLine("Riya's CGPA: " +student2.cgpa);
+            
+        }
+    }
+}
---
+++ Examples/Classes/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Student
+    {
+       public string name;
+       public DateTime dateOfBirth;
+       public double cgpa;
+       public string address;
+
+      public  double UpdateCgpa( double newCgpa)
+        {
+            cgpa = newCgpa;
+            return cgpa;
+        }
+    }
+}
