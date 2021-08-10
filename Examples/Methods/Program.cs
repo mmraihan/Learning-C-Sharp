c485284fe7d9252425fdc6663c95223dd0dc59af
mmraihan <mm_raihan@live.com>
2021-08-10 15:45:46 +0600

    Method -3  (Categories of Methods)

--- Examples/Methods/Program.cs
+++ Examples/Methods/Program.cs
@@ -4,7 +4,7 @@ namespace Methods
 {
     public class Program
     {
-
+        // Method with Argument and return Value
         public int FindMax(int num1, int num2)
         {
             int result;
@@ -19,7 +19,36 @@ namespace Methods
 
             return result;
         }
-       
+
+        // Method with No Argument and No return Value
+
+        void PrintMessage()
+        {
+            Console.WriteLine("Hello world");
+        }
+
+        // Method with  Argument and No return Value
+
+        void Sum(int num1, int num2)
+        {
+            var result = num1 + num2;
+            Console.WriteLine("Result of Sum: " +result);
+        }
+
+        // Method with No Argument But return Value
+
+         int Factorial()
+        {
+            int num = 5;
+            int fact = 1;
+
+            for (int i = 1; i <= num; i++)
+            {
+                //fact = fact *i;
+                fact *= i;
+            }
+            return fact;
+        }
 
         static void Main(string[] args)
         {
@@ -29,6 +58,15 @@ namespace Methods
             
             Console.WriteLine("Max Value: " + result);
 
-        }
+
+            maxValue.PrintMessage();
+
+            maxValue.Sum(20, 30);
+
+           var factorialResult= maxValue.Factorial();
+            Console.WriteLine("Factorial is: "+factorialResult);
+
+        
+         }
     }
 }
