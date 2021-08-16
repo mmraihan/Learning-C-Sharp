14a54fd1df42df692d5a805436ff3ca41e383f29
mmraihan <mm_raihan@live.com>
2021-08-16 15:19:44 +0600

    Method and Constructor Call

--- AlternativeByAI/AlternativeByAI/Program.cs
+++ AlternativeByAI/AlternativeByAI/Program.cs
@@ -6,7 +6,16 @@ namespace AlternativeByAI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("-------------Constructor Call------------");
+            Teacher teacher = new Teacher("Raihan from CTOR", "Male", 0145);
+            teacher.displayInformation();
+
+
+            Console.WriteLine("--------------- Method Calll---------------");
+            Teacher teacher1 = new Teacher();
+            teacher1.setInformation("Raihan", "Male", 01676);
+            teacher1.displayInformation();
         }
     }
 }
---
+++ AlternativeByAI/AlternativeByAI/Teacher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlternativeByAI
+{
+  public  class Teacher
+    {
+        public string  Name { get; set; }
+        public string  Gender { get; set; }
+        public int Phone { get; set; }
+
+        public Teacher()
+        {
+
+        }
+        public Teacher(string name, string gender, int phone) //// These parameters receive value from Main Method
+        {
+            Name = name;
+            Gender = gender;
+            Phone = phone;
+        }
+
+        public void setInformation(string name, string gender, int phone) // These parameters receive value from Main Method
+        {
+            Name = name;
+            Gender = gender;
+            Phone = phone;
+        }
+
+        public void displayInformation()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Gebder: " + Gender);
+            Console.WriteLine("Phone: " + Phone);
+            Console.WriteLine("\n");
+
+        }
+    }
+}
