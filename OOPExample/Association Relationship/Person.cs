9384f3a50fabd04eeaa8031047da9a95117dfb1d
mmraihan <mm_raihan@live.com>
2021-08-20 21:26:40 +0600

    One to one association relationship

---
+++ OOPExample/Association Relationship/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Association_Relationship
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public Address PresentAddress { get; set; }
+
+
+        public string GetFullName()
+        {
+            string fullname = $"{FirstName + " " + MiddleName + " " + LastName}";
+            return fullname;
+        }
+    }
+}
---
+++ OOPExample/Association Relationship/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Association_Relationship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address();
+            address.HouseNo = "26";
+            address.RoadNo = "2";
+            address.Area = "South Khulshi";
+
+            Person person1 = new Person();
+            person1.PresentAddress = address;
+
+            string houseNo = person1.PresentAddress.HouseNo;
+
+            Console.WriteLine(houseNo);
+            
+            person1.FirstName = "Md";
+                
+
+        }
+    }
+}
