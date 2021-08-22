baeee106de14eefaf6687233483932bd5054d1f0
mmraihan <mm_raihan@live.com>
2021-08-23 01:40:31 +0600

    Method Override

---
+++ OOPExample/MethodOverriding/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MethodOverriding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SavingAccount sv1 = new SavingAccount();
+            sv1.InterestAmount = 500;
+            sv1.AccounNo = "SV-001";
+            sv1.CustomerName = "Md. Raihan";
+            sv1.Deposit(510);
+            sv1.Withdraw(6000);
+            double balance = sv1.Balance;
+            Console.WriteLine("Saving Account: "+sv1.Withdraw(6000));
+
+            CheckingAccount chk1 = new CheckingAccount();
+            chk1.ServiceCharge = 510;
+            chk1.AccounNo = "CH-001";
+            chk1.CustomerName = "Mr Messi";
+            var result = chk1.Deposit(5010);
+            var balance2 = chk1.Withdraw(6000);
+         
+            Console.WriteLine("Checking Account: "+balance2);
+
+
+
+        }
+    }
+}
---
+++ OOPExample/MethodOverriding/SavingAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverriding
+{
+    class SavingAccount : BankAccount
+    {
+        public double InterestAmount { get; set; }
+
+        public override string Withdraw(double amount)
+        {
+            if (Balance>=amount)
+            {
+                return base.Withdraw(amount);
+            }
+            return "Insufficient Balane";
+           
+        }
+    }
+}
