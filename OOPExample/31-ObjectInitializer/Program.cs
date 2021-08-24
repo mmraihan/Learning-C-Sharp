a805608efb4566cad012b9878e339eeef842845d
mmraihan <mm_raihan@live.com>
2021-08-24 22:51:46 +0600

    31. Object Initializer

---
+++ OOPExample/31-ObjectInitializer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _31_ObjectInitializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new Student() { Id = 1, Name = "Raihan", Address = "CTG" };
+            Console.WriteLine(student.Address);
+        }
+    }
+}
