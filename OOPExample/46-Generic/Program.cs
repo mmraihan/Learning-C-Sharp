b84610a348abf377e32b1c22e837c1c11766cfe6
mmraihan <mm_raihan@live.com>
2021-08-26 14:55:59 +0600

    46. Generic Class, Method

---
+++ OOPExample/46-Generic/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _46_Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var student = new Student<int>() { Name = "Raihan", Success=1 };
+
+           var student2 = new Student<string>() { Name = "Raihan2", Success = "Passed" };
+
+           var student3 = new Student<bool>() { Name = "Raihan3", Success = true };
+
+           Print(student);
+           Print(student2);
+           Print(student3);
+           
+            Console.ReadKey();
+        }
+        public static void Print<T>(Student<T> stt)
+        {
+            Console.WriteLine($"Name: {stt.Name}, Success= {stt.Success} ");
+        }
+
+
+
+    }
+}
---
+++ OOPExample/46-Generic/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _46_Generic
+{
+   public class Student <T>
+    {
+        public string Name { get; set; }
+
+        public T Success { get; set; }
+
+
+        
+    }
+}
