dcfb748a644ef4c99780783af42597ea50d197b1
mmraihan <mm_raihan@live.com>
2021-08-27 18:49:09 +0600

    .vs

---
+++ OOPExample/0-ExtraPractise/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_ExtraPractise
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LasttName { get; set; }
+
+        public string GetFullName()
+        {
+            string fullName = $"My Full Name is {FirstName} {LasttName}";
+            return fullName;
+        }
+    }
+}
---
+++ OOPExample/0-ExtraPractise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _0_ExtraPractise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person() { FirstName = "Mubasshir", LasttName = "Raihan" };
+            
+            var result = person1.GetFullName();
+            Console.WriteLine(result);
+
+            Person person3 = person1;
+
+
+            person3.FirstName = "Md";
+
+            var result2=  person3.GetFullName();
+            var result3 = person1.GetFullName();
+
+            
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+
+            Console.ReadKey();
+        }
+    }
+}
