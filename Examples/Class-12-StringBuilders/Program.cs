0055c8baeec6a6293b10f776b97ef0b9ce00633e
mmraihan <mm_raihan@live.com>
2021-08-27 20:04:01 +0600

    Class-12 - String Builder

---
+++ Examples/Class-12-StringBuilders/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace Class_12_StringBuilders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i]=='a')
+                {
+                    
+                    Console.WriteLine("a is found");
+                }
+         
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            while (true)
+            {
+                var word = Console.ReadLine();
+                if (word == "exit")
+                    break;
+                else
+                {
+                    stringBuilder.Append("<h1>").Append(word).Append("</h1>");
+                   
+                }
+
+                Console.WriteLine(stringBuilder.ToString());
+                break;
+
+                
+            }
+
+            Console.ReadKey();
+           
+
+        }
+    }
+}
