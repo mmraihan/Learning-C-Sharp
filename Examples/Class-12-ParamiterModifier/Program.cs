6928da5d52fb6272a8810afd642233b409da49a3
mmraihan <mm_raihan@live.com>
2021-08-27 23:16:39 +0600

    Class-12 - Params with Passing an object (User define data type)

---
+++ Examples/Class-12-ParamiterModifier/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Class_12_ParamiterModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var result = sum("Mridul", 12, 3, 4);
+            Console.WriteLine(result.GetName());
+            foreach (var item in result.GetAllNums())
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+
+        public static Store sum(string name, params int [] nums)
+        {
+
+            Store store = new Store();
+            store.name = name;
+            store.SetAllNumbers(nums);
+            return store;
+           
+        }
+
+    }
+}
---
+++ Examples/Class-12-ParamiterModifier/Store.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_12_ParamiterModifier
+{
+    class Store
+    {
+        int[] numbers;
+        public string name;
+
+        public string GetName()
+        {
+            return name;
+        }
+        public  void SetAllNumbers(params int[] nums)
+        {
+            numbers = new int[nums.Length];
+            numbers = nums;            
+        }
+
+        public int[] GetAllNums()
+        {
+            return numbers;
+        }
+
+    }
+}
