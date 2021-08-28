8eef5ce868fb4971128b3a864a86626edd9fbcb5
mmraihan <mm_raihan@live.com>
2021-08-29 00:07:14 +0600

    One to Many Association Relationship

---
+++ Examples/OneToManyExample2/Department.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneToManyExample2
+{
+    class Department
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+
+        public List<Course> Courses { get; set; } = new List<Course>();
+
+        public string GetDepartmentInfo()
+        {
+            string info = $"Code: {Code}, Name: {Name} \n";
+            foreach (var item in Courses)
+            {
+                info = info + item.GetInfo();
+            }
+            return info;
+        }
+        
+    }
+}
---
+++ Examples/OneToManyExample2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OneToManyExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Course course1 = new Course();
+            course1.Code = "111";
+            course1.Title = "Basic C#";
+            course1.Credit = 3.00;
+
+            Course course2 = new Course();
+            course2.Code = "112";
+            course2.Title = "Intermediate C#";
+            course2.Credit = 3.00;
+
+            Course course3 = new Course();
+            course3.Code = "112";
+            course3.Title = "Advance C#";
+            course3.Credit = 3.00;
+
+
+            Department department = new Department();
+            department.Code = "CSE-211";
+            department.Name = "CSE";
+            department.Courses.Add(course1);
+            department.Courses.Add(course2);
+            department.Courses.Add(course3);
+
+
+            var result=department.GetDepartmentInfo();
+
+            Console.WriteLine(result);
+        }
+    }
+}
