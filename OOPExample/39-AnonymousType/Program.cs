9223253dc53950590e6da372ce70e7747b858ca0
mmraihan <mm_raihan@live.com>
2021-08-29 19:48:08 +0600

    Anonymous type Example

---
+++ OOPExample/39-AnonymousType/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _39_AnonymousType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student2 = new Student() { Id = 2 };
+
+            var student = new
+            {
+                Id = 1,
+                Name = "Raihan",
+                RegNo = "129",
+                Address = "Ctg",
+                Email = "r@example.com",
+            };
+            Console.WriteLine(student2.Id);
+            Console.WriteLine(student.Address);
+            Console.ReadKey();
+        }
+    }
+}
