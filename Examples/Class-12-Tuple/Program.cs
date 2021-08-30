70f21bb1ffee1cb3d5cab4ce82bb7bb3557646f3
mmraihan <mm_raihan@live.com>
2021-08-31 01:15:52 +0600

    Big with Tuple

---
+++ Examples/Class-12-Tuple/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Class_12_Tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
