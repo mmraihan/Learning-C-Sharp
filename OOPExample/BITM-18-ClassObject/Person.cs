ffd468da4b8409a5fc7133b11548248bc5b66883
mmraihan <mm_raihan@live.com>
2021-08-31 19:35:26 +0600

    Class and Object

---
+++ OOPExample/BITM-18-ClassObject/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BITM_18_ClassObject
+{
+    class Person
+    {
+        public string FitrstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+
+
+        public string GetFullName()
+        {
+            string FullName = $"{FitrstName} {MiddleName} {LastName}";
+            return FullName;
+        }
+
+        public string GetReverseName()
+        {
+            string FullName = GetFullName();
+
+            Char[] charArray = FullName.ToCharArray();
+            Array.Reverse(charArray);
+
+            return new string(charArray);
+        }
+
+
+    }
+}
---
+++ OOPExample/BITM-18-ClassObject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BITM_18_ClassObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.FitrstName = "Md";
+            person1.MiddleName = "Mubasshir";
+            person1.LastName = "Raihan";
+
+            person1.GetFullName();
+
+            Person person2 = new Person();
+            person2.FitrstName = "Kazi";
+            person2.MiddleName = "Bijoy";
+            person2.LastName = "Uddin";
+
+           var person2Info= person2.GetReverseName();
+            Console.WriteLine(person2Info);
+
+
+
+        }
+    }
+}
