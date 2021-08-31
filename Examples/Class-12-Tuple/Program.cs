bcdeb08263bea754ce74acf18a1a26bc6eced239
mmraihan <mm_raihan@live.com>
2021-08-31 15:43:07 +0600

    Tuple

--- Examples/Class-12-Tuple/Program.cs
+++ Examples/Class-12-Tuple/Program.cs
@@ -6,7 +6,44 @@ namespace Class_12_Tuple
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Students students= new Students();
+            //var info= students.ReturnStudentInfo();
+            //Console.WriteLine(info);
+
+            Tuple<int, string, double> obj = new Tuple<int, string, double> ( 121, "Raihan", 30000 ); // Declar with object
+
+            var emp2 = Tuple.Create(121, "Raihan2",6.2); //Decale with statci
+
+
+            var emp = GetEmployeeDetaisl();
+            Console.WriteLine($"EmpID{emp.Item1}, EmpName{emp.Item2}, EmpSalary{emp.Item3}");
+
+
+           
+
+        }
+
+        public static Tuple<int,string,double> GetEmployeeDetaisl()
+        {
+            return new Tuple<int, string, double>(1, "Raihan3", 34);
+        }
+
+     
+
         }
     }
-}
+
+    public class Students 
+    {
+        //public string Name { get; set; }
+        //public int Age { get; set; }
+
+        //public string Grade { get; set; }
+
+
+        //public (string, int, string) ReturnStudentInfo()
+        //{
+        //    return ("Raihan", 24, "A");
+        //}
+    }
