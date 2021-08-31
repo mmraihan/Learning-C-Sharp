f32c45248265cd568948a2d55cc200889fbbc3d2
mmraihan <mm_raihan@live.com>
2021-08-31 17:21:00 +0600

    Tuple

---
+++ Examples/Class-12-NullableAndAnonymous/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Class_12_NullableAndAnonymous
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = null;
+             
+            int? x = null;
+            if (x==null)
+            {
+                Console.WriteLine("X is Null");
+            }
+            x = 8;
+            if (x.HasValue)
+            {
+                Console.WriteLine(x.Value);
+            }
+
+           Program.Print();
+            
+            
+            
+        }
+        static void Print()
+        {
+            dynamic item = new { Name = "Raihan", Age = 24 };
+            Console.WriteLine(item.Name);
+        }
+    }
+}
