ad7bf7c33d4c53a85e6701bf6248377551619c30
mmraihan <mm_raihan@live.com>
2021-08-31 17:32:31 +0600

    Dictionary

---
+++ OOPExample/BITM-15-Dictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BITM_15_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Dictionary<string, double > myInfo = new Dictionary<string, double>();
+            myInfo.Add("Raihan", 1);
+            myInfo.Add("Raihan2", 2);
+            myInfo.Add("Raihan3", 3);
+            double myId = myInfo["Raihan"];
+
+            foreach (KeyValuePair <string, double> aPair in myInfo)
+            {
+                Console.WriteLine(aPair.Key+ " " +aPair.Value);
+            }
+            
+        }
+    }
+}
