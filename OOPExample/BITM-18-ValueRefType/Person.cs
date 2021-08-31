fb8e1747898ffab1c221a296a75001442e679c36
mmraihan <mm_raihan@live.com>
2021-08-31 20:39:38 +0600

    Reference and Value type

---
+++ OOPExample/BITM-18-ValueRefType/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BITM_18_ValueRefType
+{
+    class Person
+    {
+        public string FitrstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+
+
+        public string GetFullName()
+        {
+            string FullName = $"{FitrstName} {MiddleName} {LastName}";
+            return FullName;
+        }
+    }
+}
---
+++ OOPExample/BITM-18-ValueRefType/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BITM_18_ValueRefType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.FitrstName = "Md";
+            person1.MiddleName = "Mubasshir";
+            person1.LastName = "Raihan";
+
+            Person person2 = new Person();
+            person2.FitrstName = "Kazi";
+            person2.MiddleName = "Bijoy";
+            person2.LastName = "Uddin";
+
+
+            Person person3;
+            person3 = person1; // Person 1 Reference nai , kintu object ase.
+            person1 = null;
+
+            Person person4 = person3;
+
+            Change(person4);
+           
+          
+
+            Console.WriteLine(person3.GetFullName()); 
+        }
+
+        public static void Change(Person person)
+        {
+            person.LastName = "Changed";
+        }
+    }
+}
