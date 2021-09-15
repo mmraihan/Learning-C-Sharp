3c48848aee48d9cf568f5bced1a9ad7e03fc39ef
mmraihan <mm_raihan@live.com>
2021-09-15 17:05:48 +0600

    Class-16-ADONet-Code Reuse

--- Examples/Class-16-ADONet/Program.cs
+++ Examples/Class-16-ADONet/Program.cs
@@ -7,17 +7,28 @@ namespace Class_16_ADONet
     {
         static void Main(string[] args)
         {
+            var connectionString= "Server=.\\;Database=CSharpBarhch8;Trusted_Connection=True";
+
+            var insertSql = "insert into students ([name], dateOfBirth, cgpa) values ('Mahadi', '2/3/2002', 3.01) ";
+            var deleteSql = "delete from students where name='Mahadi'";
+
+            ExecuteNonQuery(connectionString,deleteSql);
+
+        }
+
+        public static void ExecuteNonQuery(string connectionString, string sql)
+        {
             using SqlConnection connection = new SqlConnection();
-            connection.ConnectionString = "Server=.\\;Database=CSharpBarhch8;Trusted_Connection=True";
+            connection.ConnectionString = connectionString;
 
             if (connection.State != System.Data.ConnectionState.Open)
             {
                 connection.Open();
             }
 
-           using SqlCommand command = new SqlCommand();
+            using SqlCommand command = new SqlCommand();
             command.Connection = connection;
-            command.CommandText = "insert into students ([name], dateOfBirth, cgpa) values ('Hasan', '2/3/2002', 3.01) ";
+            command.CommandText = sql;
             command.ExecuteNonQuery();
 
             //command.Dispose();
@@ -27,7 +38,8 @@ namespace Class_16_ADONet
             //    connection.Close();
             //}
             //connection.Dispose(); //Object Dispose
-
+            //1.24 Mins.
         }
+
     }
 }
