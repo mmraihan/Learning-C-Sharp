22dfd11f01c12397ed329469ed1fa6ff69e5f4f1
mmraihan <mm_raihan@live.com>
2021-09-23 02:20:23 +0600

    Class-18- Delegates

---
+++ Examples/Class-18-Delegates/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Class_18_Delegates
+{
+    class Program
+    {
+        delegate int method(int x, int y);
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+            method m = Formula;
+            //int r = m(2, 2);
+
+            ApplyFormula(m, 2, 2);
+            
+
+           
+           
+        }
+
+        static void ApplyFormula(method m, int a, int b)
+        {
+            Console.WriteLine(m(a,b));
+        }
+
+        static int Formula(int a, int b)
+        {
+            return a + 10 + b - 5;
+        }
+    }
+}
