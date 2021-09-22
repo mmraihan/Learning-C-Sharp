efd8db50893a79f2483637aef801ee01a2303c79
mmraihan <mm_raihan@live.com>
2021-09-22 13:41:53 +0600

    Data Structure

---
+++ OOPExample/DataStructure/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] basicSalary = new int[3];
+            basicSalary[0] = 10;
+            basicSalary[1] = 20;
+            basicSalary[2] = 30;
+
+         
+
+            int[] allowance = new int[3];
+            allowance[0] = 2;
+            allowance[1] = 2;
+            allowance[2] = 2;
+
+
+            int[] totalSalary = new int[3];
+
+            for (int i = 0; i <= 2; i++)
+            {
+                Console.WriteLine(totalSalary[i]= basicSalary[i] + allowance[i]);
+            }
+        }
+    }
+}
