3fbe49536000585ebc10ca067e99ed3a64f75650
mmraihan <mm_raihan@live.com>
2021-09-23 00:36:07 +0600

    Class-18-Directory (Folder)

--- Examples/Class-18-Files/Program.cs
+++ Examples/Class-18-Files/Program.cs
@@ -8,36 +8,50 @@ namespace Class_18_Files
     {
         static void Main(string[] args)
         {
-           /*
-            //realtive path vs absolute path
-            var fileText = File.ReadAllText("herText.txt"); //Relative path= Current Directory = where exe file is located
+            /*
+             //realtive path vs absolute path
+             var fileText = File.ReadAllText("herText.txt"); //Relative path= Current Directory = where exe file is located
 
-            var fileAbsolute = File.ReadAllText("C:\\Users\\Raihan\\Desktop\\New folder (2)\\myMtext2.txt"); //absolute path.
+             var fileAbsolute = File.ReadAllText("C:\\Users\\Raihan\\Desktop\\New folder (2)\\myMtext2.txt"); //absolute path.
 
-            var fileAgainst = File.ReadAllText("TestFile\\test2.txt"); // Basckslashh=\\ 
-            var fileAgainst2 = File.ReadAllText("TestFile/test2.txt"); // or forward slash=/ only 1
-            Console.WriteLine(fileAgainst2);
+             var fileAgainst = File.ReadAllText("TestFile\\test2.txt"); // Basckslashh=\\ 
+             var fileAgainst2 = File.ReadAllText("TestFile/test2.txt"); // or forward slash=/ only 1
+             Console.WriteLine(fileAgainst2);
 
 
-            var content = "I'm inserting some data for working with files";
-            File.WriteAllText("TestFile/newFile.txt", content); //Write file
+             var content = "I'm inserting some data for working with files";
+             File.WriteAllText("TestFile/newFile.txt", content); //Write file
 
-            var isExit=File.Exists("TestFile/newFile.txt");
-            Console.WriteLine(isExit);
+             var isExit=File.Exists("TestFile/newFile.txt");
+             Console.WriteLine(isExit);
+
+
+             string path = "TestFile/newFile2.txt";
+
+
+             using (FileStream fs = File.Create(path)) 
+             {
+                 byte[] info = Encoding.Unicode.GetBytes("This is some text in the file");
+                 fs.Write(info, 0, info.Length);
+                 fs.Flush();
+             }
             */
 
-            string path = "TestFile/newFile2.txt";
+            ////////////////Directory//////////////////
 
-
-            using (FileStream fs = File.Create(path)) 
+            var files = Directory.GetFiles("TestFile"); //Getting file from Current Folder
+            foreach (var file in files)
             {
-                byte[] info = Encoding.Unicode.GetBytes("This is some text in the file");
-                fs.Write(info, 0, info.Length);
+                Console.WriteLine(file);
             }
 
+           // Directory.CreateDirectory("TestNewDicrect"); //Crete new folder
 
 
-
+            if (Directory.Exists("TestNewDicrect")) //Delete folder
+            {
+                Directory.Delete("TestNewDicrect");
+            }
         }
     }
 }
