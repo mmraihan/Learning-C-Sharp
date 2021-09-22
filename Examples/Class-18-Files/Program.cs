117c8fab9080e628c159929ec3cab396a800fd58
mmraihan <mm_raihan@live.com>
2021-09-22 15:59:27 +0600

    Class-18- read File - absolute and relative path

---
+++ Examples/Class-18-Files/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Class_18_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //realtive path vs absolute path
+            var fileText = File.ReadAllText("herText.txt"); //Relative path= Current Directory = where exe file is located
+
+            var fileAbsolute = File.ReadAllText("C:\\Users\\Raihan\\Desktop\\New folder (2)\\myMtext2.txt"); //absolute path.
+
+            var fileAgainst = File.ReadAllText("TestFile\\test2.txt"); // Basckslashh=\\ 
+            var fileAgainst2 = File.ReadAllText("TestFile/test2.txt"); // or foeward slash=/ only 1
+            Console.WriteLine(fileAgainst2);
+
+
+
+
+            
+
+        }
+    }
+}
