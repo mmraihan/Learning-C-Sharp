34b19937bcf518947fdd563cd9ea60f9340454f2
mmraihan <mm_raihan@live.com>
2021-09-22 23:30:46 +0600

    Class-18-File stream- Create

--- Examples/Class-18-Files/Program.cs
+++ Examples/Class-18-Files/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace Class_18_Files
 {
@@ -7,19 +8,35 @@ namespace Class_18_Files
     {
         static void Main(string[] args)
         {
+           /*
             //realtive path vs absolute path
             var fileText = File.ReadAllText("herText.txt"); //Relative path= Current Directory = where exe file is located
 
             var fileAbsolute = File.ReadAllText("C:\\Users\\Raihan\\Desktop\\New folder (2)\\myMtext2.txt"); //absolute path.
 
             var fileAgainst = File.ReadAllText("TestFile\\test2.txt"); // Basckslashh=\\ 
-            var fileAgainst2 = File.ReadAllText("TestFile/test2.txt"); // or foeward slash=/ only 1
+            var fileAgainst2 = File.ReadAllText("TestFile/test2.txt"); // or forward slash=/ only 1
             Console.WriteLine(fileAgainst2);
 
 
+            var content = "I'm inserting some data for working with files";
+            File.WriteAllText("TestFile/newFile.txt", content); //Write file
+
+            var isExit=File.Exists("TestFile/newFile.txt");
+            Console.WriteLine(isExit);
+            */
+
+            string path = "TestFile/newFile2.txt";
 
 
-            
+            using (FileStream fs = File.Create(path)) 
+            {
+                byte[] info = Encoding.Unicode.GetBytes("This is some text in the file");
+                fs.Write(info, 0, info.Length);
+            }
+
+
+
 
         }
     }
