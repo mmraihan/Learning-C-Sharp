58ad5ca88f1b52c6f4776b50e83d42960c9b7614
mmraihan <mm_raihan@live.com>
2021-09-24 03:11:19 +0600

    Single Delegates, Point, Object

---
+++ OOPExample/Delegates-Single/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Delegates_Single
+{
+    class Program
+    {
+
+        public delegate int CalculatorHandler(int fNumber, int sNumber);
+
+        static void Main(string[] args)
+        {
+            //Invoke only one method
+
+            Student student = new Student();
+
+            CalculatorHandler calculatorHandler = Add; //Point
+            // int result= calculatorHandler.Invoke(10, 40); //Invoke
+            int result = calculatorHandler(2, 2);
+            Console.WriteLine(result);
+        }
+
+        static int Add (int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+       
+    }
+}
