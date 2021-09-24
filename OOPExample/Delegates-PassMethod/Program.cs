3e6b8c1fee27744b83058a580a4e9356b1f4488f
mmraihan <mm_raihan@live.com>
2021-09-24 15:41:05 +0600

    Pass method as parameter

---
+++ OOPExample/Delegates-PassMethod/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Delegates_PassMethod
+{
+    class Program
+    {
+
+        public delegate int CalculatorHandler(int fNumber, int sNumber);
+
+        static void Main(string[] args)
+        {
+            CalculatorHandler calculatorHandler = Add;
+            InvokeDelegate(calculatorHandler, 5, 5);
+
+            CalculatorHandler calculatorHandler1 = Sub;
+            InvokeDelegate(calculatorHandler1, 3, 2);
+            
+
+    
+            
+           
+        }
+
+        static int Add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        static int Sub(int firstNumber, int secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+
+
+        public static void InvokeDelegate(CalculatorHandler calculatorHandler, int fn, int sn)
+            
+        {
+            int result = calculatorHandler.Invoke(fn, sn);
+            Console.WriteLine("Output is "+ result);
+        }
+
+
+
+        
+
+    }
+}
