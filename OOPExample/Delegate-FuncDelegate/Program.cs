d7f0560000becb4c013226ab590d48d62517bfc2
mmraihan <mm_raihan@live.com>
2021-09-28 22:14:30 +0600

    Delegate Generic-1: Without anonymous method, With anonymous method, and only with return type.

---
+++ OOPExample/Delegate-FuncDelegate/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Delegate_FuncDelegate
+{ 
+    class Program
+    {
+        //Func Must be declared with Return Type.
+
+        //public delegate TResult Func<in T, out TResult>(T param); //With Declaration
+        static void Main(string[] args)
+        {
+            #region Without Lamda Method
+            /* Func<int, int, int> CalculatorHandler = Add;
+             int result = CalculatorHandler(3, 3);
+             Console.WriteLine("Func-Dlegate: " + result);
+            */
+            #endregion
+
+            #region  With Anonymous Method 
+
+            Func<int, int,int> CalculatorHandler2 = (int num1, int num2) => num1 + num2;
+            int result=CalculatorHandler2(5, 5);
+            Console.WriteLine(result);
+
+            #endregion
+
+             #region  Anonymous Method with only Return Type
+
+            Func<int> Handler= ()=>2;
+            //CW ?????
+           
+            
+           
+            #endregion
+
+            Console.ReadKey();
+        }
+
+       /* static int Add(int num, int num2)
+        {
+            return num + num2;
+        }
+       */
+    }
+}
