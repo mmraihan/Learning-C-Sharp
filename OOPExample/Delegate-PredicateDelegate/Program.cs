fc848fab749996354ece914fb6a10ede32431173
mmraihan <mm_raihan@live.com>
2021-09-28 23:41:50 +0600

    Delegate-3- Predicate delegate Incomplete.

---
+++ OOPExample/Delegate-PredicateDelegate/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Delegate_PredicateDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World 2!");
+        }
+
+    }
+}
