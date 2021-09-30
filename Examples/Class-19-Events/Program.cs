f6ee3ec9a8236e102a96821d20b3e6b778e0c424
mmraihan <mm_raihan@live.com>
2021-09-30 16:17:53 +0600

    Class-19, Topic- Events

---
+++ Examples/Class-19-Events/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Class_19_Events
+{
+    class Program
+    {
+        delegate void Receive(string receipent, string message); //Step-1
+        static event Receive Subscribe; //Step-2
+
+        static void Main(string[] args)
+        {
+            Subscribe += SMSSubscriber ; //Step-3 Adding method on Event
+
+            Subscribe += EmailSubsCriber;
+         
+            Subscribe += MMSSubscribe;
+
+            // Subscribe -= SMSSubscriber; (Remove SMS Subscriber)
+
+            Subscribe.Invoke("Raihan", "Hello! From Ctg"); //Step-3 Invoke Events
+
+            Console.ReadKey();
+        }
+
+        private static void MMSSubscribe(string receipent, string message)
+        {
+            Console.WriteLine($"Sendin MMS to: {receipent}, Message: {message}");
+        }
+
+        private static void EmailSubsCriber(string receipent, string message)
+        {
+            Console.WriteLine($"Sendin Email to: {receipent}, Message: {message}");
+        }
+
+        private static void SMSSubscriber(string receipent, string message)
+        {
+            Console.WriteLine($"Sending SMS to: {receipent}, Message: {message}");
+        }
+    }
+}
