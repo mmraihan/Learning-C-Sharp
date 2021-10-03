f999ff19bf377a817166c8892705c10c6c5fd1a8
mmraihan <mm_raihan@live.com>
2021-10-03 09:16:30 +0600

    Class-19-Expression

---
+++ Examples/Class-19-Expression/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Class_19_Expression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Expression<Func<int, int, int>> exp = (int a, int b) => a + b; //Assign Lamda method
+
+            Func<int, int, int> f = exp.Compile();
+            int result = f(2, 3);
+            Console.WriteLine(result);
+         
+           
+        }
+    } 
+}
