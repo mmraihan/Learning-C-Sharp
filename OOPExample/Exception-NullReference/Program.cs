6814cd95463873a0e79ee622191bfa646f0932d5
mmraihan <mm_raihan@live.com>
2021-10-04 17:57:26 +0600

    NULL REFERENCE EXCEPTION

---
+++ OOPExample/Exception-NullReference/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exception_NullReference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name =null;
+
+            try
+            {
+                Console.WriteLine(name.Length);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("Name can not be zero");
+                Console.WriteLine(ex.Message);
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
