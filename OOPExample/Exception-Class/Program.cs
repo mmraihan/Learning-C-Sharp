05474653f28073add509f711cd8e0a573b814e18
mmraihan <mm_raihan@live.com>
2021-10-04 18:48:09 +0600

    Exception Class

---
+++ OOPExample/Exception-Class/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exception_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //string a = null;
+                //Console.WriteLine(a.Length);
+                ///////////////////////---- 2------
+                //int[] arr = new int[2];
+                //arr[0] = 11;
+                //arr[1] = 18;
+                //arr[2] = 12;
+
+
+            }
+            catch (Exception ex) //General  (parent class )
+            {             
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
