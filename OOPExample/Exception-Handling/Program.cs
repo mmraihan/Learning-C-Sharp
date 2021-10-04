cec8d4b53729e25d59ef24fd0a485a020c0534be
mmraihan <mm_raihan@live.com>
2021-10-04 14:29:46 +0600

    DivideByZero Exception

---
+++ OOPExample/Exception-Handling/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("Enter first number: ");
+                int num1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter second number: ");
+                int num2 = int.Parse(Console.ReadLine());
+
+            try
+            {
+                int result = num1 / num2;  //Throw object
+                Console.WriteLine("Division Result is: " + result);
+            }
+            catch (DivideByZeroException ex) // asiigned object in this reference
+            {
+
+                Console.WriteLine("You can not divide number by zero...");
+                Console.WriteLine(ex.Message);
+            }
+
+              
+               
+
+                Console.WriteLine("Remaining statement 2");
+                Console.WriteLine("Remaining statement 3");
+                Console.ReadLine();   
+        }
+    }
+}
