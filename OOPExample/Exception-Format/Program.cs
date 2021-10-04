9dcf43c5c0abe6a11c47dcf7b36be10ed203c847
mmraihan <mm_raihan@live.com>
2021-10-04 18:31:37 +0600

    Handling Format Exception

---
+++ OOPExample/Exception-Format/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exception_Format
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            string number = Console.ReadLine();
+
+            try
+            {
+                int num = int.Parse(number);
+                Console.WriteLine("Number is " + num);
+            }
+            catch ( FormatException ex)
+            {
+
+                Console.WriteLine("You cant put strig");
+                Console.WriteLine(ex.Message);
+            }
+
+        
+           
+            Console.ReadKey();
+        }
+    }
+}
