d42e9b1d55ef72f9338bd8389f8cf4f6b208aa45
mmraihan <mm_raihan@live.com>
2021-10-10 17:49:23 +0600

    Reflection

---
+++ OOPExample/Reflection/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new Student();
+
+            var studentClassInfo = typeof(Student);
+
+            //string assembleyName = studentClassInfo.Assembly.FullName;
+            //Console.WriteLine(assembleyName);
+
+            //var methods = studentClassInfo.GetMethods();
+
+            //foreach (var item in methods)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            var properties = studentClassInfo.GetProperties(BindingFlags.NonPublic|BindingFlags.Instance|BindingFlags.Public);
+            foreach (var item in properties)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOPExample/Reflection/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string NAme { get; set; }
+        public string Address { get; set; }
+        public string RegNo { get; set; }
+
+        private string PhoneNumber { get; set; }  
+
+        public string GetInfo()
+        {
+            return $"Id: {Id}, Name: {NAme}, Address: {Address}, RegNo: {RegNo}";
+        }
+    }
+}
